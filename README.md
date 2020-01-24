3bfde85fbf1008b126187a69e0bdd4e840d63797
sven@schrodt-service.net <sven@schrodt-service.net>
2020-01-24 14:58:39 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# -WarrenWalter
+# WarrenWalter
+
 Test lab for neural networks inspired by Tariq Rashid's book 'Make Your Own Neural Network' 
 
 This project is named after the first names of the neural network pioneers  Warren McCulloch and Walter Pitts
---
+++ src/WarrenWalter/NeuralNetwork.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types = 1);
+/**
+ *  \WarrenWalter\NeuralNetork 
+ *  
+ *  Abstract foundation class
+ *  
+ * @package WarrenWalter
+ * @author Sven Schrodt<sven@schrodt-service.net>
+ * @version 0.1
+ * @since 2020-01-24
+ * @link https://github.com/svenschrodt/WarrenWalter
+ * @license https://github.com/svenschrodt/WarrenWalter/blob/master/LICENSE.md
+ * @copyright Sven Schrodt<sven@schrodt-service.net>
+ */
+namespace WarrenWalter;
+
+abstract class NeuralNetwork
+{
+
+    public function __construct()
+    {
+        
+    }
+    
+}
